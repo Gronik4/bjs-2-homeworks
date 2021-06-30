00cfa53975a97cfb5236e80e55b019fe342ea106
GrONik <gronweb@yandex.ru>
2021-06-30 13:54:14 +0300

    Испарвлена ошибка в tests.js и дз по массивам

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,8 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  if(arr1.length !== arr2.length) {return result = false;} 
+  result = arr1.every((item) => arr1[item] === arr2[item]);
 
   return result; // boolean
 }
@@ -9,7 +11,6 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
-
+  resultArr = arr.filter((positive) => positive >= 0).filter((item) => item % 3 === 0).map((multi) => multi * 10);
   return resultArr; // array
 }
