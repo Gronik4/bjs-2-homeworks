18ec8c7d0b40bac3774979cb49dae102a83bfdf2
GrONik <gronweb@yandex.ru>
2021-07-18 17:18:50 +0300

    ДЗ по теме декораторы

--- 7.decorators/task.js
+++ 7.decorators/task.js
@@ -1,12 +1,59 @@
 function cachingDecoratorNew(func) {
   // Ваш код
+  let cash = [];
+  return function wrupper(...args) {
+    const hash = args.join(',');
+    let idx = cash.findIndex((item) => item.hash == hash); 
+    if(idx !== -1){
+      console.log('Из кэша: ' + cash[idx].value);
+      return 'Из кэша: ' + cash[idx].value;
+    } else {
+      let resalt = func(...args);
+      cash.push({hash: [hash], value: resalt}); 
+      if(cash.length > 5) {cash.shift();} 
+      console.log('Вычисляем: ' + resalt);
+      return 'Вычисляем: ' + resalt;
+    }
+  }
+  return wrupper;
 }
 
 
-function debounceDecoratorNew(func) {
+function debounceDecoratorNew(func, ms) {
   // Ваш код
+  let isThrottled = false, savedArgs, savedThis;
+  return function(...args){
+    savedArgs = args;
+    savedThis = this;
+
+    if(isThrottled){return;}
+    func.apply(this, savedArgs);
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+      func.apply(savedThis, savedArgs);
+    }, ms)
+  }  
 }
 
-function debounceDecorator2(func) {
+function debounceDecorator2(func, ms) {
   // Ваш код
+  let isThrottled = false, savedArgs, savedThis, count = [];
+  function wrapper(...args){
+    savedArgs = args;
+    savedThis = this;
+    count.push(args); 
+
+    if(isThrottled){return;}
+    func.apply(this, savedArgs);
+    isThrottled = true;
+
+    setTimeout(() => {
+      isThrottled = false;
+      func.apply(savedThis, savedArgs);
+      console.log('Количество вызовов: ' + count.length);
+    }, ms);  
+  }
+  return wrapper;
 }
